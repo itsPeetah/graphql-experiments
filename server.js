7d31d42db58b4301b487309c77cf9abcda4d7dfa
Pietro <pietro@Peetahs-iMac-v3.local>
2021-12-30 19:52:02 +0100

    Formatting

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const app = express()
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const app = express();
+
+
+// Express middleware setup
 
 app.get('/', function (req, res){
-    res.send("Hello world!")
-})
+    res.send("Hello world!");
+});
 
 app.use("/graphql", graphqlHTTP({
     graphiql: true
-}))
+}));
 
-app.listen(5000., () => console.log("App live"))
+app.listen(5000., () => console.log("App live"));
 
