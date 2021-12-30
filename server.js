f81f2d8ee21c9a515f1ff6995c871c9336306348
Pietro <pietro@Peetahs-iMac-v3.local>
2021-12-30 19:45:26 +0100

    Implemented basic server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const app = express()
+
+app.get('/', function (req, res){
+    res.send("Hello world!")
+})
+
+app.use("/graphql", graphqlHTTP({
+    graphiql: true
+}))
+
+app.listen(5000., () => console.log("App live"))
+
