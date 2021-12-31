dfc5563694dd9a353a4597c9862c153563ee679b
Pietro <pietro@Peetahs-iMac-v3.local>
2021-12-31 20:32:31 +0100

    Setup express in new index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+import express from "express"
+
+const main =async () => {
+  
+    const app = express()
+    
+    // App routes
+    app.get("/", (_, res) => {
+        res.send("I'm live!")
+    });
+
+    app.listen(5000., () => console.log("Server started @ localhost:5000."))
+
+}
+
+main().catch((err) => {
+    console.error(err)
+})
