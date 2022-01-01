350ae6229d3204426b18ee61a2b5b31261ca2eaa
Pietro <pietro@Peetahs-iMac-v3.local>
2022-01-01 09:24:43 +0100

    Reformatted index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,11 @@ import { ArtistResolver } from "./schema/graphql/resolvers/artist";
 
 // In memory storage
 const data = {
-    artists: [
-        {id: 1, name: 'Bad Religion'},
-        {id: 2, name: 'Rise Against'},
-        {id: 3, name: 'The Blind Monkeys'}
-    ],
+    artists: [ {id: 1, name: 'Bad Religion'}, {id: 2, name: 'Rise Against'}, {id: 3, name: 'The Blind Monkeys'} ],
     songs: [
-        {id: 1, title: 'Punk Rock Song', artistId: 1 },
-        {id: 2, title: 'Sorrow', artistId: 1},
-        {id: 3, title: 'Savior', artistId: 2},
-        {id: 4, title: 'Paper Wings', artistId: 2},
-        {id: 5, title: 'Teco', artistId: 3},
-        {id: 6, title: 'Generator', artistId: 1},
-        {id: 7, title: 'Bianca', artistId: 3},
-        {id: 8, title: 'Merce Marcia', artistId: 3},
-        {id: 9, title: 'Drones', artistId: 2},
+        {id: 1, title: 'Punk Rock Song', artistId: 1 }, {id: 2, title: 'Sorrow', artistId: 1}, {id: 3, title: 'Savior', artistId: 2},
+        {id: 4, title: 'Paper Wings', artistId: 2}, {id: 5, title: 'Teco', artistId: 3}, {id: 6, title: 'Generator', artistId: 1},
+        {id: 7, title: 'Bianca', artistId: 3}, {id: 8, title: 'Merce Marcia', artistId: 3}, {id: 9, title: 'Drones', artistId: 2},
     ]
 }
 
