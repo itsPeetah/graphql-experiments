eb791e0631bd66eb41d1f4baa6ce5b206cfa2b13
Pietro <pietro@Peetahs-iMac-v3.local>
2022-01-01 15:14:13 +0100

    Implemented the remaining CRUD operations in SongResolver

--- src/schema/graphql/resolvers/song.ts
+++ src/schema/graphql/resolvers/song.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, FieldResolver, Int, Query, Resolver, Root } from "type-graphql";
+import { Arg, Ctx, FieldResolver, Int, Mutation, Query, Resolver, Root } from "type-graphql";
 import { Artist } from "../../entities/Artist";
 import { Song } from "../../entities/Song";
 import { GQLContext } from "../context";
@@ -7,18 +7,44 @@ import { GQLContext } from "../context";
 export class SongResolver{
     @Query(() => [Song])
     songs( @Ctx() {data} : GQLContext ){
-        return data.songs
+        return data.songs;
     }
     
     @Query(() => Song, {nullable: true})
     song( @Arg("id", () => Int) id : number, @Ctx() {data}: GQLContext ){
-        const song = data.songs.find(thesong => thesong.id === id)
+        const song = data.songs.find(thesong => thesong.id === id);
         return song;
     }
     
     @FieldResolver(() => Artist, {nullable: true})
-    async artist( @Root() song: Song, @Ctx() {data}: GQLContext ){
+    artist( @Root() song: Song, @Ctx() {data}: GQLContext ){
         const artist = data.artists.find(theartist => theartist.id === song.artistId);
-        return artist
+        return artist;
+    }
+
+    @Mutation(() => Song)
+    addSong( @Arg("title", () => String) title: string, @Arg("artistId", () => Int) artistId: number, @Ctx() {data} : GQLContext ){
+        const song = {title: title, artistId: artistId, id: data.songs.length + 1};
+        data.songs.push(song);
+        return song;
+    }
+
+    @Mutation(() => Boolean)
+    changeSongTitle(@Arg("id", () => Int) id: number, @Arg("newTitle", () => String) newTitle: string, @Ctx() {data} : GQLContext){
+        let success = false;
+        data.songs.forEach((thesong) => {
+            if (thesong.id === id){
+                success = true;
+                thesong.title = newTitle;
+            }
+        });
+        return success;
+    }
+
+    @Mutation(()=>Boolean)
+    deleteSong(@Arg("id", () => Int) id : number, @Ctx() {data} : GQLContext){
+        let prevLen = data.songs.length
+        data.songs = data.songs.filter(thesong => thesong.id != id);
+        return prevLen !== data.songs.length
     }
 }
