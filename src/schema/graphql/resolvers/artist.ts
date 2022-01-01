56d45372232521bbb0a42b33b584eba2bf0beb18
Pietro <pietro@Peetahs-iMac-v3.local>
2022-01-01 09:12:23 +0100

    Cleaned formatting on resolvers

--- src/schema/graphql/resolvers/artist.ts
+++ src/schema/graphql/resolvers/artist.ts
@@ -5,26 +5,18 @@ import { GQLContext } from "../context";
 @Resolver(_=>Artist)
 export class ArtistResolver{
     @Query(() => [Artist])
-    artists(
-        @Ctx() {data} : GQLContext
-    ){
+    artists( @Ctx() {data} : GQLContext){
         return data.artists
     }
     
     @Query(() => Artist, {nullable: true})
-    artist(
-        @Arg("id", () => Int) id : number,
-        @Ctx() {data}: GQLContext
-    ){
+    artist(@Arg("id", () => Int) id : number, @Ctx() {data}: GQLContext){
         const artist = data.artists.find(theartist => theartist.id === id)
         return artist;
     }
 
     @FieldResolver()
-    songs(
-        @Root() artist: Artist,
-        @Ctx() {data} : GQLContext
-    ) {
+    songs( @Root() artist: Artist,@Ctx() {data} : GQLContext){
         const songs = data.songs.filter(thesong => thesong.artistId === artist.id)
         return songs
     }
--- src/schema/graphql/resolvers/song.ts
+++ src/schema/graphql/resolvers/song.ts
@@ -6,27 +6,18 @@ import { GQLContext } from "../context";
 @Resolver(_=>Song)
 export class SongResolver{
     @Query(() => [Song])
-    songs(
-        @Ctx() {data} : GQLContext
-    ){
+    songs( @Ctx() {data} : GQLContext ){
         return data.songs
     }
     
     @Query(() => Song, {nullable: true})
-    song(
-        @Arg("id", () => Int) id : number,
-        @Ctx() {data}: GQLContext
-    ){
+    song( @Arg("id", () => Int) id : number, @Ctx() {data}: GQLContext ){
         const song = data.songs.find(thesong => thesong.id === id)
         return song;
     }
     
     @FieldResolver(() => Artist, {nullable: true})
-    async artist(
-        @Root() song: Song,
-        @Ctx() {data}: GQLContext
-    ){
-        console.log(song)
+    async artist( @Root() song: Song, @Ctx() {data}: GQLContext ){
         const artist = data.artists.find(theartist => theartist.id === song.artistId);
         return artist
     }
